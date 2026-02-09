4859128cfb8e293102ce9c5d70c82592ae2cb657
gladiatordan <dholland@theboys-db.com>
2026-02-08 21:51:01 -0800

    ok literal fresh start

--- website/frontend/src/main.jsx
+++ website/frontend/src/main.jsx
@@ -1,20 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 
-// Import the Auth Context we created
-import { AuthProvider } from './context/AuthContext.jsx'
-
-// Import the Global Theme Variables
-import './styles/variables.css' 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
